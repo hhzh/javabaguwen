ed828b236cfa8f7f01de34cca90bde609728def6
Yideng <oointer@163.com>
2024-05-02 23:31:47 +0800

    back

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,21 @@
+import {navbar} from "vuepress-theme-hope";
+
+export default navbar([
+    "/",
+    "/demo/",
+    {
+        text: "Java八股文",
+        icon: "eight",
+        link: "/eight/",
+    },
+    {
+        text: "面试流程",
+        icon: "interview",
+        link: "/interview/",
+    },
+    {
+        text: "工具",
+        icon: "tool",
+        link: "/tool/",
+    },
+]);
