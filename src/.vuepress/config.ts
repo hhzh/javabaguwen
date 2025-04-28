d0c802a46e343824d4a543df841d599fd0333393
Yideng <oointer@163.com>
2025-04-28 10:36:11 +0800

    back

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/javabaguwen/",
 
   lang: "zh-CN",
   title: "Java八股文网",
